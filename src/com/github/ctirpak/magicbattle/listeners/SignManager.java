8e9731d6f128aad6bf399751048071e47e174fc8
Chris Tirpak <ctirpak1@gmail.com>
2019-08-03 16:31:12 -0400

    sign manager wip

--- src/com/github/ctirpak/magicbattle/listeners/SignManager.java
+++ src/com/github/ctirpak/magicbattle/listeners/SignManager.java
@@ -2,10 +2,19 @@ package com.github.ctirpak.magicbattle.listeners;
 
 import java.util.HashMap;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.block.Sign;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
 
 import com.github.ctirpak.magicbattle.Arena;
+import com.github.ctirpak.magicbattle.ArenaManager;
+import com.github.ctirpak.magicbattle.LocationUtil;
+import com.github.ctirpak.magicbattle.MessageManager;
+import com.github.ctirpak.magicbattle.MessageManager.MessageType;
+import com.github.ctirpak.magicbattle.SettingsManager;
 
 public class SignManager implements Listener {
 	/*
@@ -25,9 +34,40 @@ public class SignManager implements Listener {
 		 *       x:
 		 *       y:
 		 *       z:
-		 *     arenaNumber: 1
+		 *     arenaNumber:
+		 *       1:
 		 *     
 		 */
+		
+		for(String str : SettingsManager.getLobbySigns().<ConfigurationSection>get("signs").getKeys(true)) {
+			ConfigurationSection section = SettingsManager.getLobbySigns().get("signs." + str);
+			
+			Location loc = LocationUtil.locationFromConfig(section.getConfigurationSection("location"), false);
+			Sign s = (Sign) loc.getBlock().getState();
+			Arena a = ArenaManager.getInstance().getArena(section.getInt("arenaNumber"));
+			
+			signs.put(s, a);
+		}
+	}
+	
+	public void onSignChange(SignChangeEvent e) {
+		if(e.getLine(0).equalsIgnoreCase("[MagicBattle]")) {
+			int id;
+			try {
+				id = Integer.parseInt(e.getLine(1));
+			} catch (Exception ex) {
+				MessageManager.getInstance().msg(e.getPlayer(), MessageType.BAD, "That is not a valid arena number!");
+				return;
+			}
+			
+			Arena a = ArenaManager.getInstance().getArena(id);
+			if(a == null) {
+				MessageManager.getInstance().msg(e.getPlayer(), MessageType.BAD, "That is not a valid arena!");
+				return;
+			}
+			
+			
+		}
 	}
 
 }
